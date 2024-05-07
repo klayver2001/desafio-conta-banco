47408810382b33077c7ada8720844c88ad0b729c
klayver2001 <klayveralencar2001@gmail.com>
2024-05-07 17:31:45 -0300

    Finalizado projeto ContaBanco como proposto no desafio.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
+
 public class ContaTerminal {
+    /**
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Olá, Galerinha !");
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita o numero da Agencia como String
+
+        System.out.println("Digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        // Solicita o numero da Conta como Int
+        System.out.println("Digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        // Solicita o nome do cliente em String
+        System.out.println("Por favor, digite o seu nome:");
+        String nomeCliente = scanner.nextLine();
+
+        // Solicita o saldo como Double
+        System.out.println("Por favor, digite o saldo da Conta:");
+        double saldo = scanner.nextDouble();
+
+        // Exibe a mensagem com os dados da conta bancária
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo é R$" + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
